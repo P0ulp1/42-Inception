ea46defb139619920cda2ae424e014eb56c25658
Paul Hautenauve <phautena@f2r6s18.clusters.42paris.fr>
2025-06-05 18:18:50 +0200

    * Added some unworking config wordpress files

---
+++ srcs/requirements/wordpress/conf/autoconfig-wp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sleep 10
+#if [ ! -f /var/www/wordpress/wp-config.php ]; then
+wp --allow-root config create --allow-root \
+		--dbname=$SQL_DATABASE \
+		--dbuser=$SQL_USER	\
+		--dbpass=$SQL_PASSWORD	\
+		--dbhost=mariadb:3306 --path='/var/www/wordpress'
+wp --allow-root core install --url=localhost --title=MyWordpressPage --admin_user=root --admin_password=toor --admin_email=test@example.com --path='/var/www/wordpress'
+#fi
+if [ -d "$DIRECTORY" ]; then
+	mkdir /run/php
+fi
