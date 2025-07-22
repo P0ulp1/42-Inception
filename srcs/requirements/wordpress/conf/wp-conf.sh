39769c874b72110bcb05203fd658b16a28b5e975
phautena <phautena@Inception.myguest.virtualbox.org>
2025-07-22 17:25:11 +0200

    * ADD

---
+++ srcs/requirements/wordpress/conf/wp-conf.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sleep 10
+if [ ! -f /var/www/wordpress/wp-config.php ]; then
+	wp config create --allow-root \
+			--dbname=$SQL_DATABASE \
+			--dbuser=$SQL_USER \
+			--dbpass=$SQL_PASSWORD \
+			--dbhost=mariadb:3306 --path='/var/www/wordpress'
+	wp --allow-root core install --path='/var/www/wordpress'
+	wp --allow-root user create --path='/var/www/wordpress'
+fi
+
+[ -d /run/php ] || mkdir /run/php
